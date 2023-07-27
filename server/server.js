387e4a25ee9dc206fa76ab0a6dd37f11d043aeac
Nathan Smith <nathansmith@Nathans-MacBook-Pro-2.local>
2023-07-27 13:37:08 -0600

    restart

--- server/server.js
+++ server/server.js
@@ -1,38 +1,54 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const path = require('node:path');
-const port = process.env.PORT || 3001;
-require('dotenv').config();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.resolve(__dirname, '../client/dist')));
-
+const path = require('path');
+const { logger } = require('./middleware/logger');
+const errorHandler = require('./middleware/errorHandler');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
+const connectDB = require('./config/dbConn');
 const mongoose = require('mongoose');
-const dbURL = 'mongodb://127.0.0.1:27017/capstoneProject';
-// const dbURL = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.CLUSTER}.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
-mongoose.connect(dbURL);
-const udb = mongoose.connection;
-udb.on('error', console.error.bind(console, 'connection error:'));
-udb.once('open', function () {
-  console.log('db connected');
-});
+const { logEvents } = require('./middleware/logger');
+const port = process.env.PORT || 3500;
 
-const studentSchema = new mongoose.Schema({
-  name: String,
-});
-const student = mongoose.model('student', studentSchema);
+console.log(process.env.NODE_ENV);
 
-app.get('/api', async (req, res) => {
-  try {
-    let object = await student.find();
-    res.send(object);
-  } catch (err) {
-    console.log(err);
+connectDB();
+
+app.use(logger);
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/', require('./routes/root'));
+app.use('/users', require('./routes/userRoutes'));
+app.use('/notes', require('./routes/noteRoutes'));
+
+app.all('*', (req, res) => {
+  res.status(404);
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+  } else if (req.accepts('json')) {
+    res.json({ message: '404 Not Found' });
+  } else {
+    res.type('txt').send('404 Not Found');
   }
-  console.log('request made');
 });
 
-app.listen(port, () => {
-  console.log(`server is listening on port ${port}`);
+app.use(errorHandler);
+
+mongoose.connection.once('open', () => {
+  console.log('Connected to the DataBase');
+  app.listen(port, () => {
+    console.log(`server running on port ${port}`);
+  });
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log(err);
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    mongoErrLog.log
+  );
 });
