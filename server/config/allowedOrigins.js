ad4f5c0ab70bf2c71630317ca544fbb7c60a21ca
Nathan Smith <nathansmith@Nathans-MacBook-Pro-2.local>
2023-08-12 21:30:33 -0600

    config render

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   'http://localhost:3500',
   'http://localhost:5173',
   'http://localhost:10000',
+  'https://capstone-86p9.onrender.com',
 ]
 
 module.exports = allowedOrigins
